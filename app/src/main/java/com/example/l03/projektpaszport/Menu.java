38fb7c58caa883dc8d9142b6512fcb968d1d68bf
mesomagik <bartlomiej2421@homail.com>
2016-10-26 16:13:17 +0200

    Fragmenty O mnie

--- app/src/main/java/com/example/l03/projektpaszport/Menu.java
+++ app/src/main/java/com/example/l03/projektpaszport/Menu.java
@@ -9,12 +9,21 @@ import android.widget.Button;
 public class Menu extends AppCompatActivity {
 
     private Button bMojeZdrowie;
+    private Button bOMnie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
         bMojeZdrowie =(Button) findViewById(R.id.bMojedrowie);
+        bOMnie = (Button) findViewById(R.id.bJa);
+
+        bOMnie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),OMnieActivity.class));
+            }
+        });
 
         bMojeZdrowie.setOnClickListener(new View.OnClickListener() {
             @Override
