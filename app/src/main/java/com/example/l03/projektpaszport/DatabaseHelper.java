3c29078ebe08ce2f37f02e45dc0a4be8c089a919
Pik <piotrknott@interia.eu>
2016-10-28 21:12:21 +0200

    Wprowadzone poprawki w bazie danych w ramach modułu SposobyKomunikacji. Dodane metody obsługujące ten moduł do DatabaseHelpera.

--- app/src/main/java/com/example/l03/projektpaszport/DatabaseHelper.java
+++ app/src/main/java/com/example/l03/projektpaszport/DatabaseHelper.java
@@ -17,7 +17,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     public DatabaseHelper(Context context) {
-        super(context, "baza", null,3 );
+        super(context, "baza", null, 3);
     }
 
     @Override
@@ -27,7 +27,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE Choroba (id_choroby INTEGER PRIMARY KEY, nazwa TEXT, opis TEXT,pierwsza_pomoc TEXT,link_youtube TEXT);");
         db.execSQL("CREATE TABLE Lekarstwo (id_lekarstwo INTEGER PRIMARY KEY, godzina TEXT, ilosc TEXT,sposob_zazycia TEXT,zdjecie TEXT);");
         db.execSQL("CREATE TABLE Preferencje (id_preferencja INTEGER PRIMARY KEY, lubie BOOLEAN, zdjecie TEXT,opis TEXT);");
-        db.execSQL("CREATE TABLE SposobyKomunikacj (informacje TEXT, jezyki TEXT, charakterystyczne_zachowania TEXT, wyrazanie_checi_niecheci TEXT, przekazywanie_stanow_emocjonalnych TEXT);");
+        db.execSQL("CREATE TABLE SposobyKomunikacji (moje_zmysly TEXT, charakterystyczne_zachowania TEXT, przekazywanie_emocji TEXT);");
         db.execSQL("CREATE TABLE WazneInformacje (moje_zmysly TEXT, przyjmowanie_jedzenia TEXT, przyjmowanie_plynów TEXT, moje_bezpieczenstwo TEXT, korzystanie_z_toalety TEXT, opieka_osobista TEXT, sen TEXT, alergie TEXT);");
 
     }
@@ -39,14 +39,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS Choroba");
         db.execSQL("DROP TABLE IF EXISTS Lekarstwo");
         db.execSQL("DROP TABLE IF EXISTS Preferencje");
-        db.execSQL("DROP TABLE IF EXISTS SposobyKomunikacj");
+        db.execSQL("DROP TABLE IF EXISTS SposobyKomunikacji");
         db.execSQL("DROP TABLE IF EXISTS WazneInformacje");
 
         onCreate(db);
 
     }
 
-    public long createOsoba( Integer id_osoba,String imie_nazwisko,String zdjecie,String informacje,String data_ur,String kontakt,String relacja) {
+    public long createOsoba(Integer id_osoba, String imie_nazwisko, String zdjecie, String informacje, String data_ur, String kontakt, String relacja) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -63,7 +63,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-
     public int updateOsoba(Osoba osoba) {
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -76,7 +75,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put("relacja", osoba.getRelacja());
 
         Integer wynik = db.update("Osoba", values, "id=" + osoba.getId_osoba().toString(), null);
-        Log.e("wynik edycji",wynik.toString());
+        Log.e("wynik edycji", wynik.toString());
         return wynik;
 
     }
@@ -84,18 +83,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void deleteOsoba(Osoba osoba) {
         SQLiteDatabase db = this.getWritableDatabase();
 
-        db.delete("Osoba","id="+osoba.getId_osoba().toString(),null );
+        db.delete("Osoba", "id=" + osoba.getId_osoba().toString(), null);
 
     }
 
-    public List<Osoba> getAllOsoba(){
+    public List<Osoba> getAllOsoba() {
         List<Osoba> osoby = new ArrayList<>();
-        String query = "select * from Osoba" ;
+        String query = "select * from Osoba";
 
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor c = db.rawQuery(query, null);
 
-        if(c.moveToFirst()) {
+        if (c.moveToFirst()) {
             do {
                 Osoba u = new Osoba(
                         c.getInt(c.getColumnIndex("id_osoba")),
@@ -112,14 +111,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return osoby;
     }
 
-    public List<Osoba> getAllOsobaByRelacja(String relacja){
+    public List<Osoba> getAllOsobaByRelacja(String relacja) {
         List<Osoba> osoby = new ArrayList<>();
-        String query = "select * from Osoba where relacja="+relacja ;
+        String query = "select * from Osoba where relacja=" + relacja;
 
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor c = db.rawQuery(query, null);
 
-        if(c.moveToFirst()) {
+        if (c.moveToFirst()) {
             do {
                 Osoba u = new Osoba(
                         c.getInt(c.getColumnIndex("id_osoba")),
@@ -129,13 +128,43 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                         c.getString(c.getColumnIndex("data_ur")),
                         c.getString(c.getColumnIndex("kontakt")),
                         c.getString(c.getColumnIndex("relacja")));
-                    osoby.add(u);
+                osoby.add(u);
             } while (c.moveToNext());
             c.moveToFirst();
         }
         return osoby;
     }
 
+    public long createSposobyKomunikacji(String moje_zmysly, String charakterystyczne_zachowania, String przekazywanie_emocji) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("moje_zmysly", moje_zmysly);
+        values.put("charakterystyczne_zachowania", charakterystyczne_zachowania);
+        values.put("przekazywanie_emocji", przekazywanie_emocji);
+
+        long id = db.insert("SposobyKomunikacji", null, values); //zwraca id
+        return id;
+    }
+
+    public int updateSposobyKomunikacji(SposobyKomunikacji sposobyKomunikacji) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("moje_zmysly", sposobyKomunikacji.getMoje_zmysly());
+        values.put("charakterystyczne_zachowania", sposobyKomunikacji.getCharakterystyczne_zachowania());
+        values.put("przekazywanie_emocji", sposobyKomunikacji.getPrzekazywanie_emocji());
+
+        Integer wynik = db.update("SposobyKomunikacji", values, "id=0", null);
+        Log.e("wynik edycji", wynik.toString());
+        return wynik;
+    }
+
+    public void deleteSposobyKomunikacji(SposobyKomunikacji sposobyKomunikacji) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete("SposobyKomunikacji", "id=0", null);
+    }
 
 
 }
